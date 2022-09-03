d0c10166c7386e588e92cf4e9e151a1d5d2d696e
IsaiahB <isaiahbell333@gmail.com>
2022-09-03 18:52:34 -0500

    Fixed error caused by unused import

--- client/src/routes/home/home.jsx
+++ client/src/routes/home/home.jsx
@@ -1,7 +1,6 @@
 import { useContext, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import userService from '../../api/user-service';
 import { UserContext } from '../../context/user.context';
 import { initializeComments } from '../../reducers/comment-reducer';
 
